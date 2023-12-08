ffc8509da96e7fc7e80cb3acd6c7d2f3641bd0d7
Anandagadugin <gaduginanand984@gmail.com>
2023-12-08 21:59:30 +0530

    Editing and deleting

--- src/components/expenses/AddExpenses.js
+++ src/components/expenses/AddExpenses.js
@@ -1,11 +1,42 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const AddExpenses = () => {
     const [money, setMoney] = useState('');
     const [description, setDescription] = useState('');
     const [category, setCategory] = useState('Food');
+    const [expensesList, setExpensesList] = useState([]);
 
+    useEffect(() => {
+        fetchExpenses();
+    }, []);
+
+    const fetchExpenses = async () => {
+        try {
+            const res = await axios.get(
+                'https://expense-tracker-main-e0bd5-default-rtdb.firebaseio.com/expenses.json',
+            );
+            if (res.status === 200) {
+                const data = res.data;
+
+                const loadedExpenses = [];
+                for (const key in data) {
+                    const parsedData = JSON.parse(data[key].body);
+                    loadedExpenses.unshift({
+                        id: key,
+                        money: parsedData.money,
+                        description: parsedData.description,
+                        category: parsedData.category,
+                    });
+                }
+                setExpensesList(loadedExpenses);
+            } else {
+                alert('Something went wrong, please refresh the page.');
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
 
     const handleFormSubmit = async (e) => {
         e.preventDefault();
@@ -13,8 +44,8 @@ const AddExpenses = () => {
             const expenses = {
                 money: money,
                 description: description,
-                category: category
-            }
+                category: category,
+            };
             try {
                 const resp = await axios.post(
                     'https://expense-tracker-main-e0bd5-default-rtdb.firebaseio.com/expenses.json',
@@ -24,43 +55,30 @@ const AddExpenses = () => {
                             'content-type': 'application/json',
                         },
                     }
-                )
+                );
                 if (resp.status === 200) {
-                    const res = await axios.get(
-                        'https://expense-tracker-main-e0bd5-default-rtdb.firebaseio.com/expenses.json',
-                    )
-                    if (res.status === 200) {
-                        const data = res.data
-
-                        const loadArray = []
-                        for (const key in data) {
-                            const parsedData = JSON.parse(data[key].body)
-                            loadArray.unshift({
-                                id: key,
-                                money: parsedData.money,
-                                description: parsedData.description,
-                                category: parsedData.category,
-                            })
-                        }
-                        console.log(loadArray)
-                    } else {
-                        alert('Something went wrong please Refresh Page...')
-                    }
+                    setExpensesList((prevExpenses) => [
+                        {
+                            id: resp.data.name,
+                            money: expenses.money,
+                            description: expenses.description,
+                            category: expenses.category,
+                        },
+                        ...prevExpenses,
+                    ]);
                 } else {
-                    alert('Something went wrong...')
+                    alert('Something went wrong...');
                 }
             } catch (error) {
-                window.alert('Please try again...')
-                console.log(error.message)
+                console.log(error.message);
             }
-            setCategory('Food')
-            setDescription('')
-            setMoney('')
+            setCategory('Food');
+            setDescription('');
+            setMoney('');
         } else {
-            alert('Please enter all details...')
+            alert('Please enter all details.');
         }
-    }
-
+    };
     return (
         <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-md shadow-md">
             <h2 className="text-2xl font-semibold mb-6">Add Expense</h2>
@@ -122,6 +140,32 @@ const AddExpenses = () => {
                     Add Expense
                 </button>
             </form>
+            <div className="mt-6">
+                <h3 className="text-xl font-semibold mb-3">Expense List</h3>
+                <ul className="space-y-4">
+                    {expensesList.map((expense) => (
+                        <li key={expense.id} className="border p-4 rounded-md flex justify-between items-center">
+                            <div>
+                                <p className="text-gray-600 p-2">{`Amount: ${expense.money}`}</p>
+                                <p className="text-gray-600 p-2">{`Description: ${expense.description}`}</p>
+                                <p className="text-gray-600 p-2">{`Category: ${expense.category}`}</p>
+                            </div>
+                            <div className="flex space-x-4">
+                                <button
+                                    className="text-blue-500 hover:text-blue-700 focus:outline-none"
+                                >
+                                    Edit
+                                </button>
+                                <button
+                                    className="text-red-500 hover:text-red-700 focus:outline-none"
+                                >
+                                    Delete
+                                </button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 };
